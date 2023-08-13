e7d0c5043476c1eb74a1bb611d04e6c7862cfbd7
ARFA79 <arfa79lg@gmail.com>
2023-08-13 18:02:41 +0330

    modify hash and add binary tree

--- Files/Google_practices/Intermediate/udacity_google_AL_DS/String_Keys_Practice.py
+++ Files/Google_practices/Intermediate/udacity_google_AL_DS/String_Keys_Practice.py
@@ -1,52 +1,55 @@
-"""Write a HashTable class that stores strings
-in a hash table, where keys are calculated
-using the first two letters of the string."""
-
 class HashTable(object):
     def __init__(self):
-        self.table = [[] for _ in range(1000)]
+        self.table = [None] * 10000  # create 10000 empty place for hash values
+
     def store(self, string):
-        """Input a string that's stored in 
-        the table."""
+        """Input a string that's stored in the table."""
         hash_value = self.calculate_hash_value(string)
-        bucket_index = (hash_value) % 1000
-        insert= self.table[bucket_index].append(hash_value)
-        return print('successful')
+        bucket_index = hash_value % len(self.table)
+
+        # If the bucket is empty, create a list to store items
+        if self.table[bucket_index] is None:
+            self.table[bucket_index] = []    # when its empty we are make it equal to [] 
+
+        # Append the string to the bucket
+        self.table[bucket_index].append(string)   # append the hash value we are looking for in that index
+
     def lookup(self, string):
-        """Return the hash value if the
-        string is already in the table.
+        """Return the hash value if the string is already in the table.
         Return -1 otherwise."""
         hash_value = self.calculate_hash_value(string)
-        bucket_index = (hash_value) % 1000
-        for s in self.table[bucket_index] :
-             if s == string :
-                return s
+        bucket_index = hash_value % len(self.table)  # finding the index of what we are looking for
+
+        # If the bucket is not empty, check if the string is present
+        if self.table[bucket_index] is not None and string in self.table[bucket_index]:
+            return hash_value
+
         return -1
 
     def calculate_hash_value(self, string):
-        """Helper function to calulate a
-        hash value from a string."""
-        if string :
-            hash_string = sum(ord(char) for char in string) % 1000
+        """Helper function to calculate a hash value from a string."""
+        if string:
+            hash_string = (ord(string[0]) * 100) + ord(string[1]) # claculate the hash string value . * 100 is for preventing collisions
             return hash_string
-    
+        return -1
+
 # Setup
 hash_table = HashTable()
 
 # Test calculate_hash_value
 # Should be 8568
-print (hash_table.calculate_hash_value('UDACITY'))
+print(hash_table.calculate_hash_value('UDACITY'))
 
 # Test lookup edge case
 # Should be -1
-print (hash_table.lookup('UDACITY'))
+print(hash_table.lookup('UDACITY'))
 
 # Test store
 hash_table.store('UDACITY')
 # Should be 8568
-print (hash_table.lookup('UDACITY'))
+print(hash_table.lookup('UDACITY'))
 
 # Test store edge case
 hash_table.store('UDACIOUS')
 # Should be 8568
-print (hash_table.lookup('UDACIOUS'))
+print(hash_table.lookup('UDACIOUS'))
